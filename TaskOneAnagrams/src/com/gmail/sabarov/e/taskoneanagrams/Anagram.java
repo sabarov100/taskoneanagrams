e0fadeb35e3a0a794bfb639f54643bef09a54c70
Evgeny Sabarov <e.sabarov@gmail.com>
2021-10-05 13:17:26 +0300

    refactoring: remove import and throws IOException

--- TaskOneAnagrams/src/com/gmail/sabarov/e/taskoneanagrams/Anagram.java
+++ TaskOneAnagrams/src/com/gmail/sabarov/e/taskoneanagrams/Anagram.java
@@ -1,12 +1,10 @@
 package com.gmail.sabarov.e.taskoneanagrams;
 
-import java.io.IOException;
-
 import java.util.Scanner;
 
 public class Anagram {
 	
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Reverse reverse = new Reverse();
         reverse.reverseOfWordsExpectForSymbols(scanner.nextLine());
